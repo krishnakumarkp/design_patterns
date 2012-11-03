46debef49b0894faaed5372db1b67ac081c503a0
parvathygovind <kpkrishnakumar@hotmail.com>
2012-11-03 07:59:42 -0700

    initial commit for strategy pattern

---
+++ strategy/duck.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of duck
+ *
+ * @author parvathy
+ */
+class duck {
+    //put your code here
+}
+
+?>
